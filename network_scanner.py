fc8f8684d815a007bf502c033c9ff749815fbaca
LinuxSoul <lucasnahuelguerra@gmail.com>
2021-03-13 06:10:40 -0500

    Parse list and print the result

--- network_scanner.py
+++ network_scanner.py
@@ -4,10 +4,14 @@ import scapy.all as scapy
 
 
 def scan(ip):
-    arp_request = scapy.ARP(psdt=ip)
+    arp_request = scapy.ARP(pdst=ip)
     broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
     final_request = broadcast/arp_request
-    print(final_request.summary())
+    ans_list = scapy.srp(final_request, timeout=1, verbose=False)[0]
+    print("IP\t\t\tMAC ADDRESS\n---------------------------------------------")
+
+    for element in ans_list:
+        print(element[1].psrc + "\t\t" + element[1].hwsrc)
 
 
 scan("192.168.43.1/24")
