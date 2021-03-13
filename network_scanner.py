6071f1275369aa532913fa2affdf5ac912f7ee32
LinuxSoul <lucasnahuelguerra@gmail.com>
2021-03-13 05:26:48 -0500

    Create brodcast to set MAC address destination

--- network_scanner.py
+++ network_scanner.py
@@ -5,7 +5,9 @@ import scapy.all as scapy
 
 def scan(ip):
     arp_request = scapy.ARP(psdt=ip)
-    print(arp_request.summary())
+    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+    final_request = broadcast/arp_request
+    print(final_request.summary())
 
 
 scan("192.168.43.1/24")
